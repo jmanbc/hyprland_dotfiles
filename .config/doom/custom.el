636a83634df9f144fcc537b4f4ab0d1d1501930f
jmboles <james@boleshub.com>
2023-04-18 22:04:09 -0400

    Add doom config folder

---
+++ .config/doom/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rainbow-identifiers color-identifiers-mode ivy-rtags helm-rtags ccls opencl-mode modern-cpp-font-lock disaster demangle-mode cuda-mode company-glsl cmake-mode flycheck-rtags company-rtags rtags eglot flycheck-irony company-irony-c-headers company-irony irony-eldoc irony image-dired+ beacon))
+ '(warning-suppress-types
+   '((doom-first-file-hook)
+     (doom-first-file-hook)
+     (doom-first-file-hook)
+     (doom-first-file-hook)
+     (doom-first-file-hook)
+     (doom-first-file-hook)
+     (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
